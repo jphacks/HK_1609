295972b7f46ec9b14a75ca2265413d6c8c22b7be
shomitarai <shomitarai@yahoo.co.jp>
2016-11-05 12:29:43 +0900

    Fix file path

--- main.sh
+++ main.sh
@@ -3,6 +3,6 @@ DATE=`date +'%Y%m%H%M%S'`
 
 python /home/pi/cameraTest/camera.py $DATE
 
-MP4Box -fps 30 -add /home/pi/DumpVideo/${DATE}.h264 /home/pi/VideoLogs/${DATE}.mp4
+MP4Box -fps 30 -add ./DumpVideo/${DATE}.h264 ./Video/${DATE}.mp4
 
-dropbox_uploader.sh -k upload ./VideoLogs/${DATE}.mp4 /HackVideo/${DATE}.mp4
+dropbox_uploader.sh -k upload ./Video/${DATE}.mp4 /HackVideo/${DATE}.mp4
--- raspberrypi/camera/camera.py
+++ raspberrypi/camera/camera.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 	args = sys.argv
 	camera = PiCamera()
 	camera.start_preview()
-	camera.start_recording('/home/pi/DumpVideo/'+args[1]+'.h264')
+	camera.start_recording('./../../DumpVideo'+args[1]+'.h264')
 	sleep(10)
 	camera.stop_recording()
 	camera.stop_preview()
