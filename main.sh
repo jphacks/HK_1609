3894da600b56f053f99c9126c7aba52ebb3404e6
shomitarai <shomitarai@yahoo.co.jp>
2016-11-05 23:00:32 +0900

    Fix Client secrets

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ python ./raspberrypi/camera/camera.py $DATE
 
 MP4Box -fps 30 -add ./raspberrypi/camera/h264video/${DATE}.h264 ./raspberrypi/camera/mp4video/${DATE}.mp4
 
-ID=`python ./raspberrypi/youtube/upload_video.py --file="./raspberrypi/camera/mp4video/${DATE}.mp4" --title="Summer vacation in California" --description="Had a great time surfing in Santa Cruz" --keywords="${DATE}" --category="22" --privacyStatus="private"`
+ID=`python ./raspberrypi/youtube/upload_video.py --file="./raspberrypi/camera/mp4video/${DATE}.mp4" --title="${DATE}" --description="Had a great time surfing in Santa Cruz" --keywords="${DATE}" --category="22" --privacyStatus="private"`
 
 
 python ./raspberrypi/twitter/twitter.py $DATE $ID
