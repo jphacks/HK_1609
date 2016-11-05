6ac45b51668fe1c2d1911caebac7ce4c4be2cbce
shomitarai <shomitarai@yahoo.co.jp>
2016-11-05 17:12:07 +0900

    Upload and tweet test

--- main.sh
+++ main.sh
@@ -5,6 +5,7 @@ python ./raspberrypi/camera/camera.py $DATE
 
 MP4Box -fps 30 -add ./DumpVideo/${DATE}.h264 ./Video/${DATE}.mp4
 
-dropbox_uploader.sh -k upload ./Video/${DATE}.mp4 /HackVideo/${DATE}.mp4
+ID=`python upload_video.py --file="/Users/violuka/documents/HK_1609/testvideo/201605230058.mp4" --title="Summer vacation in California" --description="Had a great time surfing in Santa Cruz" --keywords="surfing,Santa Cruz" --category="22" --privacyStatus="private"`
 
-python ./raspberrypi/twitter/twitter.py $DATE
+
+python ./raspberrypi/twitter/twitter.py $DATE $ID
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
+DATE=`date +'%Y%m%H%M%S'`
 ID=`python ./raspberrypi/youtube/upload_video.py --file="/Users/violuka/documents/HK_1609/testvideo/201605230058.mp4" --title="Summer vacation in California" --description="Had a great time surfing in Santa Cruz" --keywords="surfing,Santa Cruz" --category="22" --privacyStatus="private"
 `
 
-echo $ID
+python ./raspberrypi/twitter/twitter.py $DATE $ID
