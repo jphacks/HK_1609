ee54eec85e9e16a24d7b411dc28f501a0d6784d5
shomitarai <shomitarai@yahoo.co.jp>
2016-11-06 10:24:12 +0900

    Debug

--- raspberrypi/twitter/twitter.py
+++ raspberrypi/twitter/twitter.py
@@ -8,12 +8,11 @@ import sys
 args = sys.argv
 
 #twitter authentication
-CONSUMER_KEY = 'IFMXZ9BqqWVTmX8srd0WalFBk'
-CONSUMER_SECRET = 'bRF1LweheqbpFXUw53E4n8RsmFxtvsOprdfIkuvQEsdNzXlOMM'
-ACCESS_KEY = '794768347095134208-n1ab71hhTrjX2SVCnJ4Ut4vWgvt8cKo'
-ACCESS_SECRET = 'RCBHf2SIbiOnSuf0a54bdLXA3j4dbdV48XCng9WlsIM7L'
+CONSUMER_KEY = 'ENTER YOUR CONSUMER_KEY'
+CONSUMER_SECRET = 'ENTER YOUR CONSUMER_SECRET'
+ACCESS_KEY = 'ENTER YOUR ACCESS_KEY'
+ACCESS_SECRET = 'ENTER YOUR ACCESS_SECRET'
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
 
-
 # Do tweet
-api.update_status(status= args[1]+':aaaaa\n'+'https://www.youtube.com/watch?v='+args[2])
+api.update_status(status= args[1]+'動画が投稿されたにゃ\n'+'https://www.youtube.com/watch?v='+args[2])
