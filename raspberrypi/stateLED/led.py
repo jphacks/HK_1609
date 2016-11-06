2b012ed418faf3dbdb699387b78cff8ae7464e20
shomitarai <shomitarai@yahoo.co.jp>
2016-11-06 12:24:54 +0900

    ADD accelerometer decision

--- raspberrypi/stateLED/led.py
+++ raspberrypi/stateLED/led.py
@@ -1,11 +1,60 @@
 # -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
 import time
+import math
+import spidev
+import csv
+
+# Analog-Digital Converter
+class MCP3208:
+        def __init__(self, spi_channel=0):
+                self.spi_channel = spi_channel
+                self.conn = spidev.SpiDev(0, spi_channel)
+                self.conn.max_speed_hz = 1000000 # 1MHz
+
+        def __del__( self ):
+                self.close
+
+        def close(self):
+                if self.conn != None:
+                        self.conn.close
+                        self.conn = None
+
+        def bitstring(self, n):
+                s = bin(n)[2:]
+                return '0'*(8-len(s)) + s
+
+        def read(self, adc_channel=0):
+                # build command
+                cmd  = 128 # start bit
+                cmd +=  64 # single end / diff
+                if adc_channel % 2 == 1:
+                        cmd += 8
+                if (adc_channel/2) % 2 == 1:
+                        cmd += 16
+                if (adc_channel/4) % 2 == 1:
+                        cmd += 32
+
+                # send & receive data
+                reply_bytes = self.conn.xfer2([cmd, 0, 0, 0])
+
+                #
+                reply_bitstring = ''.join(self.bitstring(n) for n in reply_bytes)
+                # print reply_bitstring
+
+                # see also... http://akizukidenshi.com/download/MCP3204.pdf (page.20)
+                reply = reply_bitstring[5:19]
+                return int(reply, 2)
+
+
+spi = MCP3208(0)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.OUT)
 GPIO.setup(21, GPIO.IN)
 
-while GPIO.input(21)!=1:
+# for Neco Jarashi Sensor
+while spi.read(0) < 2048:
     GPIO.output(26, True)
     time.sleep(0.5)
     GPIO.output(26, False)
